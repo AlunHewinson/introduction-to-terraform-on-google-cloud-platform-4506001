f80e3b8805dd9a7c252ddcfefc3ac2e0b729e397
Alun Hewinson <alunhewinson@gmail.com>
2024-05-29 16:46:37 +0100

    Update main.tf - testing

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ data "google_compute_image" "ubuntu" {
   family      = "ubuntu-2204-lts"
 }
 
-resource "google_compute_instance" "web" {
-  name         = "web"
+resource "google_compute_instance" "web_alun" {
+  name         = "web_alun"
   machine_type = "e2-micro"
 
   
