5ceb6d2be64d6d53b2bda98b258d5222644ba448
Alun Hewinson <alunhewinson@gmail.com>
2024-05-31 14:41:42 +0100

    fix variable types

--- variables.tf
+++ variables.tf
@@ -1,39 +1,39 @@
 variable "project_id" {
-    type = "string"
+    type = string
     default = "terraform-424814"
 }
 
 variable "region" {
-    type = "string"
+    type = string
     default = "us-west1"
 }
 
 variable "network_name" {
-    type = "string"
+    type = string
     default = "app"
 }
 
 variable "network_ip_range" {
-    type = "string"
+    type = string
     default = "10.2.0.0/16"
 }
 
 variable "image_project" {
-    type = "string"
+    type = string
     default = "ubuntu-os-cloud"
 }
 
 variable "image_family" {
-    type = "string"
+    type = string
     default = "ubuntu-2204-lts"
 }
 
 variable "app_name" {
-    type = "string"
+    type = string
     default = "blog"
 }
 
 variable "machine_type" {
-    type = "string"
+    type = string
     default = "e2-micro"
 }
